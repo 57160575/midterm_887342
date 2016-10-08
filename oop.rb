a75213f3bc8703ee1e7380031bd45cf75d6ba1b7
57160575 <ts_prank@hotmail.com>
2016-10-08 16:00:14 +0700

    Create oop.rb

---
+++ oop.rb
@@ -0,0 +1,15 @@
+class Language
+  def initialize(name, creator)
+    @name = name
+    @creator = creator
+  end
+	
+  def description
+    puts "I'm #{@name} and I was created by #{@creator}!"
+  end
+end
+
+
+ruby.description
+python.description
+javascript.description
